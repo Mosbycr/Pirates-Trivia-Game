f18c58057a58bde43337c761f9de04111ae326e9
Mosbycr <mosbycr@gmail.com>
2019-03-11 22:39:28 -0400

    js linked and question object created, need to add image links

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+    var pirateQuestions = [
+      {
+        question:
+          "The rules that pirates lived by while on ship were known as?",
+        choices: ["Guidelines", "Articles", "Laws"],
+        correct: 1,
+        image: "../images/articles.jpg"
+      },
+
+      {
+        question:
+          "Only two women were ever convicted of piracy. One was Anne Bonney, but who was the other?",
+        choices: ["Red Handed Jill", "Mary Read", "Blackbeard's Bride"],
+        correct: 1,
+        image: "",
+      },
+
+      {
+        question:
+          "What is the name of the flag generally associated with pirates?",
+        choices: ["The Jolly Roger", "Skull and Crossbones", "Hanging Man"],
+        correct: 0,
+        image: "",
+      },
+
+      {
+        question:
+          "What was Blackbeard's real name?",
+        choices: ["Johnathan Larky", "Jasper Hook", "Edward Teach"],
+        correct: 2,
+        image: "",
+      },
+
+      {
+        question:
+          "The English word pirate is derived from which of the following Latin terms?",
+        choices: ["piratos", "pyrit", "pirata"],
+        correct: 2,
+        image: "",
+      },
+
+      {
+        question:
+          "If you were entitled to a double share of the loot and booty ye would be the ____?",
+        choices: ["First Mate", "Captain", "Surgeon"],
+        correct: 1,
+        image: "",
+      },
+    ];
+
+
+    
+});
